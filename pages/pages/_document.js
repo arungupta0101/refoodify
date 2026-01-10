319e72c65e17e64417d41d72a0ba636b0848ad20
arungupta0101 <arunjacker0101@.com>
2026-01-11 02:05:05 +0530

    chore: add google site verification

---
+++ pages/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="eruoAVfU-z3MKWx_cd_WCIUtsOxMJMWWHMIO2PU0Uck"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
